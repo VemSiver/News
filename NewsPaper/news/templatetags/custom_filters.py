6994dcbf81822e265fb4bb3e67aa433d92eaef62
VemSiver <vatar8585@gmail.com>
2022-03-30 20:26:54 +0300

    D3-ht-tabl

--- NewsPaper/news/templatetags/custom_filters.py
+++ NewsPaper/news/templatetags/custom_filters.py
@@ -11,3 +11,15 @@ def multiply(value, arg):
         raise ValueError(
             f'Нельзя умножить {type(value)} на {type(arg)}')
 
+STRONG_WORDS = ["php", "редиска"]
+
+
+@register.filter()
+def censor(value):
+   if not isinstance(value, str):
+       raise ValueError('Нельзя цензурировать не строку')
+
+   for word in STRONG_WORDS:
+       value = value.replace(word[1:], '*' * (len(word)-1))
+
+   return value
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -7,14 +7,13 @@ class PostList(ListView):
     model = Post
     template_name = 'post.html'
     context_object_name = 'post'
-    queryset = Post.objects.order_by('-id')
+    queryset = Post.objects.all()
 
     # метод get_context_data нужен нам для того, чтобы мы могли передать переменные в шаблон. В возвращаемом словаре context будут храниться все переменные. Ключи этого словаря и есть переменные, к которым мы сможем потом обратиться через шаблон
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['time_now'] = datetime.utcnow()  # добавим переменную текущей даты time_now
-        context[
-            'value1'] = None  # добавим ещё одну пустую переменную, чтобы на её примере посмотреть работу другого фильтра
+        context['value1'] = None  # добавим ещё одну пустую переменную, чтобы на её примере посмотреть работу другого фильтра
         return context
 
 
@@ -22,6 +21,6 @@ class PostList(ListView):
 class PostDetail(DetailView):
     model = Post  # модель всё та же, но мы хотим получать детали конкретно отдельного поста
     template_name = 'post.html'  # название шаблона будет post.html
-    context_object_name = 'post'  # название объекта. в нём будет
+    context_object_name = 'post'  # название объекта.
 
 
